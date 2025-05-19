e482a5f80790857860eb8c09a36668605d440484
dagim-a <adgoligndagim@gmail.com>
2025-05-19 17:26:44 +0300

    Add files via upload

---
+++ template-2.js
@@ -0,0 +1,46 @@
+    window.onclick = function(event) {
+        const modal = document.querySelector('.pop-modal');
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+
+function closeModal() {
+      document.getElementById("Pop_up").style.display = "none";
+      document.getElementById("overlay").style.display = "none";
+    }
+
+
+
+
+
+
+
+
+
+
+
+function showAlertCSS() {
+    alert("CSS button have been clicked!");
+} 
+function showAlertHTML() {
+    alert("HTML button have been clicked!");
+} 
+function showAlertBOOTSTRAP() {
+    alert("BOOTSTRAP button have been clicked!");
+} 
+function showAlertRUBY() {
+    alert("RUBY button have been clicked!");
+} 
+function showAlertSEEPROJECT() {
+    alert("See Project button have been clicked!");
+} 
+function showAlertGETMYRESUME() {
+    alert("Ger My Resume button have been clicked!");
+} 
+function showAlertSeeSource() {
+    alert("see source button have been clicked!");
+}
+function showAlertSeeLive() {
+    alert("see live button have been clicked!");
+}
